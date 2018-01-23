84be614ac7c8b7c61566ccf5ab9b51c28cfea378
starlc <starlc_2588@163.com>
2018-01-23 09:15:02 +0800

    简单代理模式

---
+++ src/main/java/com/starlc/designmode/proxy/simple/Proxyer.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.starlc.designmode.proxy.simple;
+
+
+/**
+ * ¥˙¿Ì»À
+ * @author liucheng2
+ *
+ */
+public class Proxyer implements IAct {
+	private Singer singer;
+	
+	public Proxyer(Singer singer) {
+		this.singer = singer;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.starlc.designmode.proxy.simple.IAct#sing()
+	 */
+	@Override
+	public void sing() {
+		singer.sing();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.starlc.designmode.proxy.simple.IAct#dance()
+	 */
+	@Override
+	public void dance() {
+		// TODO Auto-generated method stub
+		singer.dance();
+	}
+
+}
